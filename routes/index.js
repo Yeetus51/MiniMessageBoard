537e5ae3261383c6b4e1480de02cd4ea4feb0867
Omair <513589@student.saxion.nl>
2023-10-24 13:52:41 +0200

    Files

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: "Mini Messageboard", messages: messages })
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: "Mini Messageboard", messages: messages })
+});
+
+router.post("/new", function(req,res,next){
+
+  console.log(req); 
+  let messageText = req.body.messageText; 
+  let messageUser = req.body.messageUser; 
+  messages.push({text: messageText, user: messageUser, added: new Date()});
+  res.redirect('/');
+}); 
+
+module.exports = router;
+
+
+
